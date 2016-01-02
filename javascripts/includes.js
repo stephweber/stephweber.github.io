d7e816b984a2b2b9d85ff9df235ba69a8bbb0fae
Colin Fuller <colin@khanacademy.org>
2016-01-02 13:04:54 -0800

    Add a join us section

--- javascripts/includes.js
+++ javascripts/includes.js
@@ -1,53 +1,52 @@
-
-function load_content(contentname) {
-  $("#included-content").load(contentname + ".html");
+function loadContent(contentname) {
+    $("#included-content").load(contentname + ".html");
 }
 
-function section_names() {
-  return ['home', 'research', 'teaching', 'publications', 'cv', 'contact'];
+function sectionNames() {
+    return ['home', 'research', 'teaching', 'publications',
+            'cv', 'contact', 'join-us'];
 }
 
 function deactivate() {
-  var sections = section_names();
-  for (var i = 0; i < sections.length; i++) {
-    $("#" + sections[i]).removeClass("active");
-  }
-}
-
-function do_section(name) {
-  deactivate();
-  $("#" + name).addClass("active");
-  $("#" + name + "-link").blur();
-  load_content(name);
-}
-
-function create_click_function(name) {
-  return function() {
-    if (window.location.href.search("index\\.html") == -1) {
-      window.history.pushState("", "", "/index.html");
+    var sections = sectionNames();
+    for (var i = 0; i < sections.length; i++) {
+        $("#" + sections[i]).removeClass("active");
     }
-    window.location.search = "?" + name;
-    do_section(name);
-  }
+}
+
+function doSection(name) {
+    deactivate();
+    $("#" + name).addClass("active");
+    $("#" + name + "-link").blur();
+    loadContent(name);
+}
+
+function createClickFunction(name) {
+    return function() {
+        if (window.location.href.search("index\\.html") === -1) {
+            window.history.pushState("", "", "/index.html");
+        }
+        window.location.search = "?" + name;
+        doSection(name);
+    };
 }
 
 function onLoad() {
-  var section = window.location.search.substring(1); // strip leading ?
-  if (section == "") {
-    do_section("home");
-  } else {
-    do_section(section);
-  }
+    var section = window.location.search.substring(1); // strip leading ?
+    if (section === "") {
+        doSection("home");
+    } else {
+        doSection(section);
+    }
 }
 
 $(function() {
-  var sections = section_names();
-  for (var i = 0; i < sections.length; i++) {
-    $("#" + sections[i] + "-link").click(create_click_function(sections[i]));
-  }
-  onLoad();
-})
-
+    var sections = sectionNames();
+    for (var i = 0; i < sections.length; i++) {
+        $("#" + sections[i] + "-link").click(
+            createClickFunction(sections[i]));
+    }
+    onLoad();
+});
 
 window.onpopstate = onLoad;
-
